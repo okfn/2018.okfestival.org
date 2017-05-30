2dc7d090897bfff8203da56968f8664aee681aed
Sam Smith <sam@mintcanary.com>
2017-05-30 18:17:19 +0100

    WIP

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'assets/scss',
+          src: ['*.scss'],
+          dest: 'assets/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    cssmin: {
+      options: {
+        sourceMap: true
+      },
+      target: {
+        files: {
+          'assets/css/style.css': ['node_modules/normalize.css/normalize.css', 'assets/css/main.css']
+        }
+      }
+    },
+
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			},
+      cssmin: {
+				files: '**/*.css',
+				tasks: ['cssmin']
+			}
+		}
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default',['watch']);
+
+};
